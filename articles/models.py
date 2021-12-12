94089e7ea7e457c84fc4881ba1357cb3dcd157b9
Igor Teleutsa <igor.teleutsa@gameloft.com>
2021-12-12 17:38:32 +0200

    added authorization

--- articles/models.py
+++ articles/models.py
@@ -8,7 +8,7 @@ from ckeditor.fields import RichTextField
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = RichTextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDeleteView, ArticleUpdateView, ArticleDetailView
+from .views import ArticleListView, ArticleDeleteView, ArticleUpdateView, ArticleDetailView, ArticleCreateView
 
 urlpatterns = [
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name="article_edit"),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name="article_delete"),
     path('<int:pk>/detail/', ArticleDetailView.as_view(), name="article_detail"),
